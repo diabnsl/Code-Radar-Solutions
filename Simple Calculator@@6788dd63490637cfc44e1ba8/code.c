f2e998c9b9b7e3eec2eae1e1254c05369d8cbfe7
diabnsl <diyab9292@gmail.com>
2025-02-04 11:13:23 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    float x,y;
+    char opt;
+    scanf("%f %f", &x, &y);
+    scanf("%c", &opt);
+    switch(opt){
+        case '+':
+            printf("%.2f", x+y);
+            break;
+        case '-':
+            printf("%.2f", x-y);
+            break;
+        case '*':
+            printf("%.2f", x*y);
+            break;
+        case '/':
+            printf("%.2f", x/y);
+            break;
+            
+    }
+    
+    return 0;
+}
