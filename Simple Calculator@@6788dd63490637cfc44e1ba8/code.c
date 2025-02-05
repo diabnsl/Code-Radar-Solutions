f8886356919c6df1c3037362f01ce95804ee5bc5
diabnsl <diyab9292@gmail.com>
2025-02-05 12:46:06 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 int main() {
     float x,y;
-    float a,b,c,d;
     char opt;
     scanf("%f %f", &x, &y);
-    scanf("%c", &opt);
+    scanf(" %c", &opt);
     switch(opt){
         case '+':
-            printf("%.2f", x+y);
+            printf("%.2f", x + y);
             break;
         case '-':
-            printf("%.2f", x-y);
+            printf("%.2f", x - y);
             break;
         case '*':
-            printf("%.2f", x*d);
+            printf("%.2f", x * d);
             break;
         case '/':
-            printf("%.2f", x/y);
+            printf("%.2f", x / y);
             break;
+        default:
+            printf("Invalid operator");
             
     }
     
