c197fadb498578272a3728234fe341cade5fb254
diabnsl <diyab9292@gmail.com>
2025-02-05 12:48:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@ int main() {
     float x,y;
     char opt;
     scanf("%f %f", &x, &y);
-    scanf(" %c", &opt);
+    scanf("%c", &opt);
     switch(opt){
         case '+':
             printf("%.2f", x + y);
